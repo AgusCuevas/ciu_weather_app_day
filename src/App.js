caeea2d068e2068f433add8a69fe3df449729b2c
AgusCuevas <cuevasagustin168@gmail.com>
2021-12-26 17:29:06 -0300

    2.0

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import './App.css';
 import React from 'react';
-import { useState } from 'react';
 import Ciudad from './components/Ciudad';
 
-function App() {
-  
-  let key = '591e65570d9b021b9b330b9d36a1bc61'
-  const [ciudad, setCiudad] = useState([])
-  const consultarAPI = async () => {
-    try{
-      const api = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=-34.5708&lon=-58.6243&exclude=current,hourly&appid=${key}`);
-      const ciudad = await api.json();
-      setCiudad(ciudad.result);
-      } catch (error) {
-        console.log(error);
-    }
-  }
-
+function App() { 
   return (
     <div className="container">
       <header>
       <img className='cabecera' src="https://resizer.glanacion.com/resizer/J0cCJjYfXh_-iPQ2U1cS4_HOGPE=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/BAEK27MGRZBXPEU6C6XIBUOCCI.jpg"/>
       </header>
       <div className="col">
-        {consultarAPI}
         <Ciudad
-          ciudad= {ciudad}
         />
       </div>
     </div>
@@ -33,3 +17,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/Ciudad.jsx
+++ src/components/Ciudad.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Ciudad = ({ ciudad }) => {
-    return ( 
-        <div align="center" className="border border-info" style="max-width: 18rem;" style= {
+function Ciudad() {
+
+    const [ubi, setUbi] = useState([])
+    const [temp, setTemp]  = useState([]);
+    const [hum, setHum]  = useState([]);
+    const [viento, setViento]  = useState([]);
+    const [nubes, setNubes]  = useState([]);
+
+    let key = '591e65570d9b021b9b330b9d36a1bc61'
+
+    const guardarDatos = async () => {
+        let ciudad = document.getElementById('busqueda').value;
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}`)
+            .then(response => {return response.json()})
+            .then (data => {
+                console.log(ciudad);
+                console.log(data);
+                setUbi(data.name);
+                setTemp(Math.round(data.main.temp - 273.15));
+                setHum(Math.round(data.main.humidity));
+                setViento(Math.round(data.wind.speed * 1.609));
+                setNubes(Math.round(data.clouds.all))
+        })
+    }
+    return (      
+        <div 
+        align="center" className="border border-info" style="max-width: 18rem;" style= {
             {
                 fontFamily: 'cursive',
                 fontSize: '200%',
@@ -16,38 +40,49 @@ const Ciudad = ({ ciudad }) => {
                 borderBottomWidth: '5px',
             } 
         }>  
+            <div>
+            <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand">Navbar</a>
+                <form className="d-flex">
+                <input className="form-control me-2"  placeholder="Search" aria-label="Search" id = 'busqueda'/>
+                <button className="btn btn-outline-success" type="button" onClick={guardarDatos}>Search</button>
+                </form>
+            </div>
+            </nav>
+            </div>
             <div className="border border-info">
                 <p></p> 
                 <p> <img className = 'img' src = 'https://w7.pngwing.com/pngs/792/99/png-transparent-computer-icons-international-mother-language-day-location-black-and-white-line.png' /> </p>
-                Ubicacion: Tessei, Buenos Aires
+                Ubicacion: {ubi}
                 <p></p>
             </div>
 
             <div className="border border-info">
                 <p></p>
                 <p><img className = 'img' src = 'https://previews.123rf.com/images/jemastock/jemastock1710/jemastock171004157/87574139-term%C3%B3metro-temperatura-icono-imagen-vector-ilustraci%C3%B3n-dise%C3%B1o-blanco-y-negro.jpg'/> </p>
-                Temperatura: {ciudad.temp}
+                Temperatura: {temp}°
                 <p></p>
             </div>
 
             <div className="border border-info">
                 <p></p>
                 <p><img className = 'img' src = 'https://thumbs.dreamstime.com/b/icono-del-vector-del-porcentaje-de-la-humedad-ejemplo-blanco-y-negro-de-la-humedad-alta-icono-linear-s%C3%B3lido-del-tiempo-92782975.jpg'/> </p>
-                Humedad: {ciudad.humidity}
+                Humedad: {hum}%
                 <p></p>
             </div>
 
             <div className="border border-info">
                 <p></p>
                 <p><img className = 'img' src = 'https://w7.pngwing.com/pngs/877/400/png-transparent-computer-icons-wind-weather-wind-text-meteorology-black-and-white.png'/> </p>
-                Viento: {ciudad.wind}
+                Viento: {viento} k/m
                 <p></p>
             </div>
 
             <div className="border border-info">
                 <p></p>
                 <p><img className = 'img' src = 'https://illustoon.com/photo/2731.png'/> </p>
-                Nubes: {ciudad.clouds}
+                Nubes: {nubes}%
                 <p></p>
             </div>
         </div>
